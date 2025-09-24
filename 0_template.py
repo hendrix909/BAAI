0dc7e1ecb9967669f326701ade78299e9a282e25
hendrix909 <infinity.flow777@gmail.com>
2025-09-24 21:57:17 +0800

    upload practice file

--- 0_template.py
+++ 0_template.py
@@ -6,6 +6,7 @@
 
 # 1. Input
 
+
 # 2. Process
 
 # 3. Output
---
+++ hendrix909_Test.py
@@ -0,0 +1,16 @@
+#
+# James,2025/09//24
+# File: hendrix909.py
+# Practice 101
+#
+
+# 1. Input
+
+X = 2
+Y = 2     
+Z = 2
+
+# 2. Process
+Total = X * Y * Z + (X + Y + Z )
+# 3. Output
+print('The Correct Answer is:', Total)
